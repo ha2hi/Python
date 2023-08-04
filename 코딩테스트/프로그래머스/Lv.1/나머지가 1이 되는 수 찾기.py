2fbbadf2c09e803f56d5abf305df93587b6c4926
ha2hi <a01045542949@gmail.com>
2023-08-04 23:35:55 +0900

    2023-08-04

--- 코딩테스트/프로그래머스/Lv.1/나머지가 1이 되는 수 찾기.py
+++ 코딩테스트/프로그래머스/Lv.1/나머지가 1이 되는 수 찾기.py
@@ -1,10 +1,5 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/87389
 def solution(n):
-    result = 0
-    if n >= 3:
-        for i in range(1, n+1):
-            if n % i == 1:
-                result = i
-                break
-                
-        return result
+    for i in range(1,n+1):
+        if n % i == 1:
+            return i

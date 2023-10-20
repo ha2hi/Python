873d14610ad0ac536566c0048328e33d4f2b22ed
ha2hi <56156892+ha2hi@users.noreply.github.com>
2023-10-21 00:01:46 +0900

    2023-10-20

---
+++ 파이썬 기본/그리디/역수열.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+res = [0] * n
+for i in range(n):
+    for j in range(n):
+        if a[i] == 0 and res[j] ==0:
+            res[j] = i+1
+            break
+        elif res[j] == 0:
+            a[i] -= 1
+print(*res)

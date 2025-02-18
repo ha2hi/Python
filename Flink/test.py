5513629dd8413d5a63d0b3fc76b04fb6bbc257dc
HyukSangCho <a01045542949@gmail.com>
2025-02-18 23:32:07 +0900

    study bfs

--- Flink/test.py
+++ Flink/test.py
@@ -1,12 +1,52 @@
-from pyflink.table import EnvironmentSettings, TableEnvironment
+from pyflink.common.time import Instant
+from pyflink.common import Types
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import(
+    DataTypes,TableDescriptor, Schema, StreamTableEnvironment
+)
+from pyflink.table.expressions import lit, col 
+from pyflink.table.window import Tumble
 
-env = EnvironmentSettings.in_streaming_mode()
-table_env = TableEnvironment.create(env)
+env = StreamExecutionEnvironment.get_execution_environment()
+env.set_parallelism(1)
+t_env = StreamTableEnvironment.create(env)
 
-source = table_env.from_elements([(1, "Hi", "Hello"), (2, "Hello", "Hello")], ["a", "b", "c"])
+ds = env.from_collection(
+    collection = [
+        (Instant.of_epoch_milli(1000), "Alice", 110.1),
+        (Instant.of_epoch_milli(4000), "Bob", 30.2),
+        (Instant.of_epoch_milli(3000), "Alice", 20.0),
+        (Instant.of_epoch_milli(2000), "Bob", 53.1),
+        (Instant.of_epoch_milli(5000), "Alice", 13.1),
+        (Instant.of_epoch_milli(3000), "Bob", 3.1),
+        (Instant.of_epoch_milli(7000), "Bob", 16.1),
+        (Instant.of_epoch_milli(10000), "Alice", 20.1),
+    ],
+    type_info=Types.ROW([Types.INSTANT(), Types.STRING(), Types.FLOAT()]))
 
-# Get TableResult
-table_result = table_env.sql_query("select a + 1, b, c from %s" % source)
+table = t_env.from_data_stream(
+    ds,
+    Schema.new_builder()
+          .column_by_expression("ts", "CAST(f0 AS TIMESTAMP(3))")
+          .column("f1", DataTypes.STRING())
+          .column("f2", DataTypes.FLOAT())
+          .watermark("ts", "ts - INTERVAL '3' SECOND")
+          .build()
+).alias("ts", "name", "price")
 
-# Print the table
-table_result.execute().wait()
+t_env.create_temporary_table(
+    "sink",
+    TableDescriptor.for_connector("print")
+                   .schema(Schema.new_builder()
+                            .column("name", DataTypes.STRING())
+                            .column("total_price", DataTypes.FLOAT())
+                            .column("w_start", DataTypes.TIMESTAMP(3))
+                            .column("w_end", DataTypes.TIMESTAMP(3))
+                            .build()
+                   ).build())
+
+table = table.window(Tumble.over(lit(5).seconds).on(col("ts")).alias("w"))\
+             .group_by(table.name, col("w")) \
+             .select(table.name, table.price.sum, col("w").start, col("w").end)
+
+table.execute_insert("sink").wait()
